7e925b145e876b6488791a2edeb641dec149c3d8
bagarrattaa <apple.macos.ios.1@protonmail.com>
2022-08-26 14:15:38 +0530

    k[ljpoiu

---
+++ ios.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apk update
+apk upgrade 
+
+apk add python 
+apk add python3
+apk add libxml2
+
+pip3 install yagmail
+apkk add python3-pip
+apk add python-pip
+
+apk add git 
+pip3 install yagmail 
+pip install yagmail 
+
+cd ~
+git clone https://github.com/bagarrattaa/gmail-bomber-client
