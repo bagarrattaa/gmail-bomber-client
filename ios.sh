83f572eae2ee4db7ecb5e56b6ca6c7ddb777bb8c
bagarrattaa <apple.macos.ios.1@protonmail.com>
2022-08-30 14:23:05 +0530

    kpoojui

--- ios.sh
+++ ios.sh
@@ -2,13 +2,13 @@
 apk update
 apk upgrade 
 
-apk add python 
+# apk add python 
 apk add python3
 apk add libxml2
 
 pip3 install yagmail
 apkk add python3-pip
-apk add python-pip
+# apk add python-pip
 
 apk add git 
 pip3 install yagmail 
